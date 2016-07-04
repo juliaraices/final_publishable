abdcc42cd258c50f4f2b3247d63419283fc4ca7f
Júlia Raíces <julia.raices@gmail.com>
2016-07-04 11:14:40 -0300

    changes in control

--- control.R
+++ control.R
@@ -16,7 +16,7 @@ bootz <- rep("NA", 100)
 # 1st try
 a = 0
 b = 0
-for(i in 1:2){
+for(i in 1:100){
     repeat{
         eq <- equal
         temp <- eq[sample(nrow(eq), 200, replace = TRUE), ]
@@ -33,59 +33,59 @@ for(i in 1:2){
 
 bootz
 
-
-# 2nd try
-b = 0
-for(i in 1:100){
-    while(TRUE){
-        eq <- equal
-        temp <- eq[sample(nrow(eq), 2), ]
-        blabs <- wilcox.test(temp$Meiosis, meiotic$Meiosis)
-        b <- b+1
-        print(b)
-        if(blabs$p.value >= 0.05) break
-    }
-    will <- wilcox.test(temp$alpha, meiotic$alpha)
-    bootz <- will$p.value
-}
-
-# 3nd try
-c = 0
-random.sample <- function(interest) {
-    eq <- equal
-    x <- eq[sample(nrow(eq), length(meiotic$Meiosis)), ]
-    t <- wilcox.test(x$Meiosis, meiotic$Meiosis)
-    t$p.value >= 0.05
-    c <- c + 1
-    print(c)
-    if(t$p.value >= 0.05) return(x)
-    else Recall(x)# run the function again
-}
-
-for(i in 1:100){
-    y <- random.sample(equal)
-    will <- wilcox.test(y$alpha, meiotic$alpha)
-    bootz <- will$p.value
-}
-
-
-
-
-# 4nd try
-d = 0
-sucess <- FALSE
-for(i in 1:100){
-    while(!sucess){
-        eq <- equal
-        temp <- eq[sample(nrow(eq), length(meiotic$Meiosis)), ]
-        blabs <- wilcox.test(temp$Meiosis, meiotic$Meiosis)
-        d <- d+1
-        print(d)
-        sucess <- blabs$p.value >= 0.05
-    }
-    will <- wilcox.test(temp$alpha, meiotic$alpha)
-    bootz <- will$p.value
-}
-
-
-
+# 
+# # 2nd try
+# b = 0
+# for(i in 1:100){
+#     while(TRUE){
+#         eq <- equal
+#         temp <- eq[sample(nrow(eq), 2), ]
+#         blabs <- wilcox.test(temp$Meiosis, meiotic$Meiosis)
+#         b <- b+1
+#         print(b)
+#         if(blabs$p.value >= 0.05) break
+#     }
+#     will <- wilcox.test(temp$alpha, meiotic$alpha)
+#     bootz <- will$p.value
+# }
+# 
+# # 3nd try
+# c = 0
+# random.sample <- function(interest) {
+#     eq <- equal
+#     x <- eq[sample(nrow(eq), length(meiotic$Meiosis)), ]
+#     t <- wilcox.test(x$Meiosis, meiotic$Meiosis)
+#     t$p.value >= 0.05
+#     c <- c + 1
+#     print(c)
+#     if(t$p.value >= 0.05) return(x)
+#     else Recall(x)# run the function again
+# }
+# 
+# for(i in 1:100){
+#     y <- random.sample(equal)
+#     will <- wilcox.test(y$alpha, meiotic$alpha)
+#     bootz <- will$p.value
+# }
+# 
+# 
+# 
+# 
+# # 4nd try
+# d = 0
+# sucess <- FALSE
+# for(i in 1:100){
+#     while(!sucess){
+#         eq <- equal
+#         temp <- eq[sample(nrow(eq), length(meiotic$Meiosis)), ]
+#         blabs <- wilcox.test(temp$Meiosis, meiotic$Meiosis)
+#         d <- d+1
+#         print(d)
+#         sucess <- blabs$p.value >= 0.05
+#     }
+#     will <- wilcox.test(temp$alpha, meiotic$alpha)
+#     bootz <- will$p.value
+# }
+# 
+# 
+# 
